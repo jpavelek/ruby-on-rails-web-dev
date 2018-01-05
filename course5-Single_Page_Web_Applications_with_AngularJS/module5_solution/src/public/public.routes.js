c444c65c092d7a6e32caa29f7ab1fd504225e3ba
Jakub Pavelek <jpavelek@live.com>
2018-01-05 22:49:28 +0100

    Implement angular form validation prior to form submission

--- course5-Single_Page_Web_Applications_with_AngularJS/module5_solution/src/public/public.routes.js
+++ course5-Single_Page_Web_Applications_with_AngularJS/module5_solution/src/public/public.routes.js
@@ -43,9 +43,10 @@ function routeConfig ($stateProvider) {
     })
     .state('public.signup', {
       url: "/signup",
-      templateUrl: "src/public/signup/signup.html"
-    }
-
+      templateUrl: "src/public/signup/signup.html",
+      controller: "SignUpController",
+      controllerAs: "signupCtrl"
+      }
     );
 }
 })();
